57fddff2940dbfd82480e528140b6d36fa4df1fe
jerryko570 <jerry.narae@gmail.com>
2025-12-05 02:31:39 +0900

    feat: tailwind 기반 button  컴포넌트 UI 시스템 구축 (#14)

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,8 @@
 export default function Home() {
-  return <h1>Helllo</h1>;
+  return (
+    <div>
+      <h1>안녕하세영 ㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋ</h1>;
+      <button className="btn btn-primary btn-sm">작은</button>
+    </div>
+  );
 }
