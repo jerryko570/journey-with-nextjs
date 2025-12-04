04946776c41be05b6f2b8c01a26bb60de9c419a0
jerryko570 <jerry.narae@gmail.com>
2025-12-04 22:07:33 +0900

    feat: initial project setup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,26 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import prettier from "eslint-config-prettier";
 
-const eslintConfig = defineConfig([
+export default defineConfig([
   ...nextVitals,
   ...nextTs,
-  // Override default ignores of eslint-config-next.
+  
+  {
+    rules: {
+      "no-unused-vars": "warn",
+      "no-console": "off",
+      "react-hooks/exhaustive-deps": "warn",
+    },
+  },
+
+  prettier, // prettier와 충돌 방지
+
   globalIgnores([
-    // Default ignores of eslint-config-next:
     ".next/**",
     "out/**",
     "build/**",
     "next-env.d.ts",
   ]),
 ]);
-
-export default eslintConfig;
