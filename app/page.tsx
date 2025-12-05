ae38604871c76fe577e944e0506289641dbb504a
jerryko570 <jerry.narae@gmail.com>
2025-12-05 12:03:46 +0900

    feat: input 컴포넌트 UI 시스템 구축 (#14)

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,18 @@
 export default function Home() {
   return (
-    <div>
+    <main className="space-y-3">
       <h1>TEST</h1>
-      <button className="btn btn-primary btn-lg">큰 버튼</button>
-      <button className="btn btn-disable btn-md">중간 버튼</button>
-      <button className="btn btn-outline btn-sm">작은 버튼</button>
-    </div>
+      <div className="space-y-3">
+        <button className="btn btn-primary btn-lg">큰 버튼</button>
+        <button className="btn btn-disabled btn-md">중간 버튼</button>
+        <button className="btn btn-outline btn-sm">작은 버튼</button>
+      </div>
+
+      <div className="space-y-3">
+        <input className="input" placeholder="입력하세요"></input>
+        <input className="input" disabled placeholder="입력하세요"></input>
+        <input className="input input-error" placeholder="입력하세요"></input>
+      </div>
+    </main>
   );
 }
